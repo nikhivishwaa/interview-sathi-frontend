88a5560df333780c134e2c8d121b1f74ad5fe851
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2025-03-10 23:53:00 +0530

    setting up the project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='mt-5 text-red-500'>
+      I am Header
+    </div>
+  )
+}
+
+export default Header
---
+++ src/screens/DashboardScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function DashboardScreen() {
+  return (
+    <div>
+      <Header/>
+      <h1>Dashboard Screen</h1>
+    </div>
+  )
+}
+
+export default DashboardScreen
